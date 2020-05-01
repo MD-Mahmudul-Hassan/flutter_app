bbec5ff94ba5614b3f4851c7cc822f826901f9e3
Mahmudul Hassan <mdhassan6767@gmail.com>
2020-05-01 23:53:16 +0600

    #completed a basic scaffold structure including appBar, BackgroundImage, Center, NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Your journey begins'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
